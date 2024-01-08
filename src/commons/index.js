95eee21d59cb02cc74f2d22895f71ed8ced6a8c5
Yonatan <yon.palac@gmail.com>
2024-01-08 20:50:27 -0300

    add new project

--- src/commons/index.js
+++ src/commons/index.js
@@ -1,4 +1,5 @@
 import pokedex from "../assets/projects/pokedex.png";
+import img2 from "../assets/projects/img2.png";
 
 export const PROJECTS = [
     {
@@ -9,10 +10,10 @@ export const PROJECTS = [
         link: "https://challenge-pokedex-xi.vercel.app/"
     },
     {
-        image: pokedex,
-        name: "Pokedex",
-        tecnologies: "react - redux - Axios - styled-components - Pagination - Material-UI - animate.css",
-        repository: "https://github.com/YonPalac1/challenge_pokedex",
-        link: "https://challenge-pokedex-xi.vercel.app/"
+        image: img2,
+        name: "DEPLACE",
+        tecnologies: "react - css - magic cursor - Loading - Framer motion",
+        repository: "https://github.com/YonPalac1/deplace-landing",
+        link: "https://deplace-landing.vercel.app/"
     }
 ]
