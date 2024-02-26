7a6f8f14506af8aa1cb180f9e2b03f56c312fb88
YonPalac1 <yon.palac@gmail.com>
2024-02-26 19:44:26 -0300

    add new project

--- src/commons/index.js
+++ src/commons/index.js
@@ -1,5 +1,6 @@
 import pokedex from "../assets/projects/pokedex.png";
 import img2 from "../assets/projects/img2.png";
+import img3 from "../assets/projects/img3.png";
 
 export const PROJECTS = [
     {
@@ -15,5 +16,12 @@ export const PROJECTS = [
         tecnologies: "react - css - magic cursor - Loading - Framer motion",
         repository: "https://github.com/YonPalac1/deplace-landing",
         link: "https://deplace-landing.vercel.app/"
-    }
+    },
+    {
+        image: img3,
+        name: "Baron de la cerveza",
+        tecnologies: "react - redux - index search - cart - filters",
+        repository: "https://github.com/YonPalac1/baron-de-la-cerveza",
+        link: "https://baron-de-la-cerveza.vercel.app/"
+    },
 ]
