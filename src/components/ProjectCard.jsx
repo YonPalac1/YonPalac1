7ad6bee866f8c79feaa97a9dec33d8b65b362a31
YonPalac1 <yon.palac@gmail.com>
2024-02-27 14:01:55 -0300

    add new proyects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
-import { Container, Row, Col, Modal } from "react-bootstrap";
-import { Github, BoxArrowInUpRight, ArrowRightShort } from "react-bootstrap-icons";
+import { Container, Row, Col } from "react-bootstrap";
+import { BoxArrowInUpRight } from "react-bootstrap-icons";
 
 export const ProjectCard = ({ project }) => {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     return <Col md="4" sm="6">
         <Container>
             <Row>
                 <Col className="projects_image">
                     <div className="projects_image-img">
-                        <img onClick={handleShow} src={project.image} alt={project.name}></img>
+                        <img src={project.image} alt={project.name}></img>
                     </div>
                     <div className="project-name">
                         <h3>{project.name}</h3>
@@ -28,18 +22,6 @@ export const ProjectCard = ({ project }) => {
             </Row>
         </Container>
 
-        <Modal show={show} onHide={handleClose} centered>
-            <Modal.Header closeButton>
-                <Modal.Title>{project.name}</Modal.Title>{" "} 
-                <a href={project.link}><BoxArrowInUpRight /></a>
-            </Modal.Header>
-            <Modal.Body>
-                <img src={project.image}></img>
-                {project.tecnologies}</Modal.Body>
-            <Modal.Footer>
-                <p>Mas detalles en <ArrowRightShort />
-                <a href={project.repository} target="_blank"><Github /> </a> </p>
-            </Modal.Footer>
-        </Modal>
+        
     </Col>
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,18 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Col, Modal } from "react-bootstrap";
 import { Slide } from "react-awesome-reveal";
+import { Github, BoxArrowInUpRight } from "react-bootstrap-icons";
+
 
 import { ProjectCard } from "./ProjectCard";
 import { PROJECTS } from "../commons";
 
 export const Projects = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   return (
     <section className="project" id="projects">
@@ -25,7 +33,35 @@ export const Projects = () => {
             </div>
           </Col>
         </Row>
+        <Row>
+          <Col size={12} className="text-center">
+            <button onClick={handleShow} className="btn bg-dark text-white more-proyects mt-5">more projects?</button>
+          </Col>
+        </Row>
       </Container>
+
+      <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton>
+                <Modal.Title>more projects</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {
+                  PROJECTS.map((project, i) => {
+                    return <Container>
+                        <Row className="modal-links">
+                          <Col>
+                            <a href={project.link}>{project.name}</a>
+                          </Col>
+                          <Col className="modal-links_link">
+                          <h4><a href={project.repository} target="_blank"><Github /></a></h4>
+                          <h4><a href={project.link} target="_blank"><BoxArrowInUpRight /></a></h4>
+                          </Col>
+                        </Row>
+                      </Container>
+                  })
+                }
+            </Modal.Body>
+        </Modal>
     </section>
   )
 }
